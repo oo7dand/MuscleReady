1b53c464c7aa1c4cd93dda600712882088e331a8
oo7dand <dan.devereaux@gmail.com>
2025-08-14 09:39:35 -0400

    Add files via upload

---
+++ sw-v13.js
@@ -0,0 +1,5 @@
+const CACHE='mr-v13';
+const ASSETS=['./','./index.html?v=13','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
